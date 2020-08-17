66d43aa875290fb92518308e1d71af43b22500ae
LineageFalcon <68928999+LineageFalcon@users.noreply.github.com>
2020-08-17 18:30:32 +0200

    implemented a future feature for form controlling the grid

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ class Runtime {
 
 }
 
-function builder() {
-    let body = document.getElementsByTagName('body')[0];
+function builder(size) {
+    let body = document.getElementsByTagName('section')[0];
+    // body.style.gridTemplateColumns = size;
+    // body.style.gridTemplateRows = size;
     for(let i = 0; i < 9; i++){
         body.appendChild(document.createElement('div'));
         console.log('Ping');
