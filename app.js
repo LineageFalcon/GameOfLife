acafbdadc818a0f7556f03120f4efb773f163dbb
LineageFalcon <68928999+LineageFalcon@users.noreply.github.com>
2020-08-17 23:12:41 +0200

    made some rearangements of the code

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 class instanceOfLife {
     constructor(size) {
-        this.size = size*size;
+        this.size = size;
         this.items = [];
     }
 
+    builder() {
+        let body = document.getElementsByTagName('section')[0];
+        // body.style.gridTemplateColumns = size;
+        // body.style.gridTemplateRows = size;
+        for(let i = 0; i < this.size * this.size; i++){
+            let div = document.createElement('div');
+            if (i >= 3 && i <= 5) {
+                body.appendChild(div).setAttribute('class', 'alive');
+            }
+            else {
+                body.appendChild(div);
+            }
+        }
+    }
+
     getDivs() {
-        for(let i = 0; i < (this.size); i++) {
+        for(let i = 0; i < (this.size * this.size); i++) {
             console.log(document.getElementsByTagName('div')[i]);
             this.items[i] = document.getElementsByTagName('div')[i];
         }
@@ -15,16 +30,10 @@ class instanceOfLife {
         this.items[index].style.backgroundColor = "blue";
     }
 
+    calculateN() {
+
+    }
+
 }
 
 let runing = new instanceOfLife(3);
-
-function builder(size) {
-    let body = document.getElementsByTagName('section')[0];
-    // body.style.gridTemplateColumns = size;
-    // body.style.gridTemplateRows = size;
-    for(let i = 0; i < 9; i++){
-        body.appendChild(document.createElement('div'));
-        console.log('Ping');
-    }
-}
