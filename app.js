7d828e66b4f86a5c3e7b5353ed8464395c805a14
LineageFalcon <68928999+LineageFalcon@users.noreply.github.com>
2020-08-17 17:36:50 +0200

    Implementing builder for html tags

---
+++ app.js
@@ -0,0 +1,15 @@
+class Runtime {
+    constructor() {
+
+    }
+
+
+}
+
+function builder() {
+    const body = document.getElementsByTagName('body');
+    const div = document.createElement('div');
+    for(let i = 0; i < 10; i++){
+        
+    }
+}
