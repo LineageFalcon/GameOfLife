a378d94c0c21107521729ef4f3923a8b5755e833
LineageFalcon <68928999+LineageFalcon@users.noreply.github.com>
2020-08-20 03:50:09 +0200

    made the size adjustable and made it possible to make multiple instance of the object

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
 class instanceOfLife {
-    constructor(size = 3) {
-        this.size = size;// going to be a user input
-        this.hostSize = size * size;//used in for-loops
+    constructor(hostHeight = 3, hostWidth = 3) {
+        this.hostHeight = hostHeight;// going to be a user input
+        this.hostWidth = hostWidth;// going to be a user input
+        this.areaSize = hostHeight * hostWidth;//used in for-loops
         this.items = [];//holds the div obj from the DOM
+        this.wrapper;
         this.host= [];//will be fused to a twodimensional array
         this.futureHost = [];//same here
-        this.time;
     }
 
     builder() {
-        let body = document.getElementsByTagName('section')[0];
-        body.style.gridTemplateColumns = 'repeat(' + this.size + ', auto)';
-        body.style.gridTemplateRows = 'repeat(' + this.size + ', auto)';
-        for(let i = 0; i < this.hostSize; i++){
+        let body = document.getElementsByTagName('body')[0];
+        let section = document.createElement('section');
+        body.appendChild(section);
+        this.wrapper = section;
+        section.style.gridTemplateColumns = 'repeat(' + this.hostHeight + ', auto)';
+        section.style.gridTemplateRows = 'repeat(' + this.hostWidth + ', auto)';
+        for(let i = 0; i < this.areaSize; i++){
             let div = document.createElement('div');
             let ran = Math.round(Math.random());
             if (ran == 1) {
-                body.appendChild(div).setAttribute('class', 'alive');
+                section.appendChild(div).setAttribute('class', 'alive');
             }
             else {
-                body.appendChild(div).setAttribute('class', 'dead');
+                section.appendChild(div).setAttribute('class', 'dead');
             }
         }
     }
 
     getDivs() {
-        for(let i = 0; i < (this.hostSize); i++) {
-            this.items[i] = document.getElementsByTagName('div')[i];
+        for(let i = 0; i < (this.areaSize); i++) {
+            this.items[i] = this.wrapper.getElementsByTagName('div')[i];
         }
     }
 
-    changeColor(index = 2) {
-        this.items[index].style.backgroundColor = "blue";
-    }
-
     prepHost() {
-        for(let i = 0; i < this.size; i++) {
-            for(let k = 0; k < this.size; k++) {
+        for(let i = 0; i < this.hostHeight; i++) {
+            for(let k = 0; k < this.hostHeight; k++) {
                 this.host[i] = [];
                 this.futureHost[i] = [];
             }
@@ -44,8 +44,8 @@ class instanceOfLife {
     }
 
     copyHost() {
-        for(let i = 0; i < this.size; i++) {
-            for(let k = 0; k < this.size; k++) {
+        for(let i = 0; i < this.hostHeight; i++) {
+            for(let k = 0; k < this.hostWidth; k++) {
                 this.host[i][k] = this.futureHost[i][k];
                 this.futureHost[i][k] = 0;
             }
@@ -54,8 +54,8 @@ class instanceOfLife {
 
     mapDivs() {
         let index = 0;
-        for(let i = 0; i < this.size; i++) {
-            for(let k = 0; k < this.size; k++) {
+        for(let i = 0; i < this.hostHeight; i++) {
+            for(let k = 0; k < this.hostWidth; k++) {
                 if(this.items[index].getAttribute('class') == 'alive'){
                     this.host[i][k] = 1;
                 } else {
@@ -68,8 +68,8 @@ class instanceOfLife {
 
     formatDivs() {
         let index = 0;
-        for(let i = 0; i < this.size; i++) {
-            for(let k = 0; k < this.size; k++) {
+        for(let i = 0; i < this.hostHeight; i++) {
+            for(let k = 0; k < this.hostWidth; k++) {
                 if(this.host[i][k] == 1) {
                     this.items[index].setAttribute('class', 'alive');
                 } else {
@@ -81,31 +81,31 @@ class instanceOfLife {
     }
 
     calcN() {
-        for(let i = 0; i < this.size; i++) {
-            for(let k = 0; k < this.size; k++){
+        for(let i = 0; i < this.hostHeight; i++) {
+            for(let k = 0; k < this.hostWidth; k++){
                 let count = 0;
                 if(i - 1 >= 0) {
                     if(this.host[i - 1][k] == 1) {count++;}
                 }
-                if(i + 1 < this.size) {
+                if(i + 1 < this.hostHeight) {
                     if(this.host[i + 1][k] == 1) {count++;}
                 }
                 if(k - 1 >= 0) {
                     if(this.host[i][k - 1] == 1) {count++;}
                 }
-                if(k + 1 < this.size) {
+                if(k + 1 < this.hostWidth) {
                     if(this.host[i][k + 1] == 1) {count++;}
                 }
                 if(i - 1 >= 0 && k - 1 >= 0) {
                     if(this.host[i - 1][k - 1]) {count++;}
                 }
-                if(i - 1 >= 0 && k + 1 < this.size) {
+                if(i - 1 >= 0 && k + 1 < this.hostWidth) {
                     if(this.host[i - 1][k + 1]) {count++;}
                 }
-                if(i + 1 < this.size && k + 1 < this.size) {
+                if(i + 1 < this.hostHeight && k + 1 < this.hostWidth) {
                     if(this.host[i + 1][k + 1]) {count++;}
                 }
-                if(i + 1 < this.size && k - 1 >= 0) {
+                if(i + 1 < this.hostHeight && k - 1 >= 0) {
                     if(this.host[i + 1][k - 1]) {count++;}
                 }
                 switch(this.host[i][k]) {
@@ -129,7 +129,7 @@ class instanceOfLife {
 
 }
 
-let X = new instanceOfLife(10);
+let X = new instanceOfLife(10, 100);
 X.builder();
 X.getDivs();
 X.prepHost();// prepare methods
