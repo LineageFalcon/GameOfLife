98096e996518a1765ea515408cfae3ff97954c8e
Malcom Cook <68928999+LineageFalcon@users.noreply.github.com>
2020-09-05 17:23:47 +0200

    merge of Cleancodeconcept into master (#3)
    * my save-load shit
    
    * hot fix -> leaving working commit
    
    * 📝: correction
    
    Co-authored-by: SonicYeager <kimstone.yeager@gmail.com>

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-//import instanceOfGrid from 'gameOfLifeClass';
-let X = new instanceOfGrid(50, 50);
+let userInput = new UserInput(50, 50);
+let grid = new Grid(userInput); 
+let form = new Interface(userInput, new Form(userInput));
 
-play(X);
+play(grid, form);
 
-function play(obj, evolution = true) {
+function play(grid, form, evolution = true) {
+    let time;
     let time;
     if (evolution) {
-        obj.renderStep();
-        time = setTimeout(play, 100, obj, evolution);
+        form.renderStep(grid); //redering is done from form with params (grid)
+        time = setTimeout(play, 100, grid, form, evolution);
     } else {
         clearTimeout(time);
     }
