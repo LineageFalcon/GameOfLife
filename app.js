c957f41011603d0b9961b0108f6f0bbc077aac85
LineageFalcon <68928999+LineageFalcon@users.noreply.github.com>
2021-02-26 18:50:12 +0100

    🗑️: reordered file structure

--- app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let modelInstance = new logicRender();
-    let viewInstance = new guiRender();
-    new controller();
-
-    controller.setView(viewInstance);
-    controller.setModel(modelInstance);
-});
